3352c200d2c2f5a25bc01b74a7fff81900076041
w4nt <inkquill72@gmail.com>
2017-08-22 06:06:47 +0300

    Update shopping_list.py
    Added member functions of the class.

--- python-files/shopping_list.py
+++ python-files/shopping_list.py
@@ -1,2 +1,18 @@
 class ShoppingList (object):
     def __init__(self):
+        pass
+    
+    def __init__(self):
+        pass
+    
+    def addItem(self):
+        pass
+        
+    def viewItem(self):
+        pass
+    
+    def updateItem(self):
+        pass
+    
+    def deleteItem(self):
+        pass
